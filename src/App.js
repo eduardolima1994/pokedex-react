e505356687a4bb3bc940d9aa4fbd0fd761ea6ec7
Eduardo Lima <camposnovospa@gmail.com>
2021-12-09 22:52:39 -0300

    Atualização da Listagem

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React,{useState, useEffect} from 'react';
+import './App.css';
+
+export default function App() {
+
+  const [pokelista, setPokelista] = useState([])
+  const [pokesprite, setPokesprite] = useState([])
+
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=1118&offset=0')
+        .then(response => (response.json()))
+        .then(data => setPokelista(data.results));
+  }, []);
+
+  useEffect((pokelist = 'bulbasaur') => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokelist}/`)
+        .then(response => (response.json()))
+        .then(data => setPokesprite(data.sprites));
+
+  }, []);
+
+  const chama=()=>{
+    return(
+      <img src={pokesprite.front_default}/>
+    )
+  }
+
+  console.log(pokesprite)
+
+  return (
+    <div className="App">    
+      
+
+      { 
+      pokelista.map((results) => 
+        <div className='Poke' key={results.name}>
+          <h1>{ results.name }</h1>
+          <p>{ results.url}</p>
+          {chama()}
+        </div>) 
+      }
+      
+
+    </div>
+  );
+}
